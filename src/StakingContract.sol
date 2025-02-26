0c9a91df0ac593687c366709a0c17ad23cba6de9
JakobeG301 <jakobeg301@gmail.com>
2025-02-26 20:57:12 +0100

    [DappScout] Create Contracts and Test files

--- src/StakingContract.sol
+++ src/StakingContract.sol
@@ -1,14 +1,15 @@
 // SPDX-License-identifier: MIT
 pragma solidity ^0.8.28;
 
-import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import {ScoutToken} from "./TokenERC20.sol";
 
-contract StakingContract is Ownable, Pausable {
+contract StakingContract is Ownable(msg.sender), Pausable {
 uint256 test;
 
+constructor(address initialOwner){}
 
 
 
--- src/TokenERC20.sol
+++ src/TokenERC20.sol
@@ -10,7 +10,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ScoutToken is ERC20{
  
- 
+ constructor() ERC20("ScoutToken", "SCT"){
+    _mint(msg.sender, 10000);
+ }
 
 
 
